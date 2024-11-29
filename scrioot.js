c89d693082c2d6642e7c8bd7e89baae4fddb5d5b
belinhaLIGHT <154287268+belinhaLIGHT@users.noreply.github.com>
2024-11-28 21:23:59 -0300

    Add files via upload

---
+++ scrioot.js
@@ -0,0 +1,31 @@
+function pag7() {
+    window.location = "2-1.html"
+}
+
+function pag8() {
+    window.location = "2-2.html"
+}
+
+function pagmorrer2() {
+        window.location = "pagmorrer2.html"
+}
+
+function pag9() {
+    window.location = "2-3.html"
+}
+
+function pag10() {
+    window.location = "2-4.html"
+}
+
+function pag11() {
+    window.location = "2-5.html"
+}
+
+function pag12() {
+    window.location = "2-6.html"
+}
+
+function pag13() {
+    window.location = "2-7.html"
+}
